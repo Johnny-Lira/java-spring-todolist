f906b7572f6b054b9e83a21ecfa0b268814a4fec
Johnny-Lira <johnnyjlira@gmail.com>
2023-10-16 19:46:09 -0300

    Deploy

---
+++ todolist/src/main/java/br/com/damieleleao/todolist/filter/OncePerRequestFilter.java
@@ -0,0 +1,5 @@
+package br.com.damieleleao.todolist.filter;
+
+public interface OncePerRequestFilter {
+
+}
---
+++ todolist/src/test/java/br/com/damieleleao/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.damieleleao.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
